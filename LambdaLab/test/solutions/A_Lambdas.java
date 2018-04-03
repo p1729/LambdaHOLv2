11f9a518b886dd896a1c5f4e40ca240df82c4bf0
Stuart Marks <stuart.marks@oracle.com>
2018-04-03 16:53:54 -0700

    Clarify some of the instructions

--- LambdaLab/test/solutions/A_Lambdas.java
+++ LambdaLab/test/solutions/A_Lambdas.java
@@ -293,7 +293,7 @@ public class A_Lambdas {
     }
 
     /**
-     * Write an unbound method reference that returns a new, empty StringBuilder.
+     * Write a constructor reference that returns a new, empty StringBuilder.
      */
     @Test
     public void d_supplier3() {
