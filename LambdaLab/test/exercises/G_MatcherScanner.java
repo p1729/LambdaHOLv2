2e2fedd199be9ea0e16491aa4d2da02f32e59a5a
José <Jose.Paumard@gmail.com>
2018-11-22 15:16:45 +0100

    Fixed the test from G_MatcherScanner

--- LambdaLab/test/exercises/G_MatcherScanner.java
+++ LambdaLab/test/exercises/G_MatcherScanner.java
@@ -77,8 +77,21 @@ public class G_MatcherScanner {
         final Pattern TRIGRAPH_PAT = Pattern.compile("[aeiou]{3}", Pattern.CASE_INSENSITIVE);
         String result = null; // TODO
 
-        assertThat(result).contains("b[EAU]ty's");
-        assertThat(result.length()).isEqualTo(614);
+        String expectedResult = "From fairest creatures we desire increase,\n" +
+                "That thereby b[EAU]ty's rose might never die,\n" +
+                "But as the riper should by time decease,\n" +
+                "His tender heir might bear his memory:\n" +
+                "But thou contracted to thine own bright eyes,\n" +
+                "Feed'st thy light's flame with self-substantial fuel,\n" +
+                "Making a famine where abundance lies,\n" +
+                "Thy self thy foe, to thy sweet self too cruel:\n" +
+                "Thou that art now the world's fresh ornament,\n" +
+                "And only herald to the gaudy spring,\n" +
+                "Within thine own bud buriest thy content,\n" +
+                "And, tender churl, mak'st waste in niggarding:\n" +
+                "Pity the world, or else this glutton be,\n" +
+                "To eat the world's due, by the grave and thee.\n";
+        assertThat(result).isEqualToNormalizingNewlines(expectedResult);
     }
     // Hint:
     // <editor-fold defaultstate="collapsed">
