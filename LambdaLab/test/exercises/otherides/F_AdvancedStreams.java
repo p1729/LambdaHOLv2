d44eda9da9f54d92e390d65fd0cca0b13f0ff76d
mauricen <maurice@morninglight.co.uk>
2018-12-03 11:23:43 +0530

    Improve comments

--- LambdaLab/test/exercises/otherides/F_AdvancedStreams.java
+++ LambdaLab/test/exercises/otherides/F_AdvancedStreams.java
@@ -234,7 +234,7 @@ public class F_AdvancedStreams {
 
         String result = input.collect(null, null, null).toString();
         // TODO fill in lambda expressions or method references
-        // in place of the nulls in the line above.
+        // TODO in place of the nulls in the line above.
 
         assertThat(result).isEqualTo("tsrqponmlkjihgfedcbaabcdefghijklmnopqrst");
     }
@@ -257,12 +257,12 @@ public class F_AdvancedStreams {
     // </editor-fold>
 
     /**
-     * Count the total number of words and the number of distinct, lower case
-     * words in a stream, in one pass. This exercise uses a helper class
-     * that defines methods that are called by the Stream.collect() method.
-     * Your task is to fill in the implementation of the accumulate() and
-     * combine() methods in the helper class. You don't need to modify the
-     * test method itself.
+     * f7: Count the total number of words and the number of distinct, lower
+     * case words in a stream, in one pass. This exercise uses a helper class
+     * TotalAndDistinct that defines methods that are called by the method
+     * Stream.collect(). Your task is to fill in the implementation of the
+     * accumulate() and combine() methods in the helper class. You don't need
+     * to modify the test method itself.
      *
      * The stream is run in parallel, so you must write a combine() method
      * that works properly.
@@ -294,7 +294,7 @@ public class F_AdvancedStreams {
     public void f7_countTotalAndDistinctWords() {
         List<String> allWords = reader.lines()
                                       .map(String::toLowerCase)
-                                      .flatMap(line -> SPLIT_PATTERN.splitAsStream(line))
+                                      .flatMap(SPLIT_PATTERN::splitAsStream)
                                       .collect(Collectors.toList());
 
         TotalAndDistinct totalAndDistinct =
